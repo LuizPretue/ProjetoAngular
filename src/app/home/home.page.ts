c0bb60faae6dd53ce5aa2e8055ce8b14c9acf87b
LuizFCSantos <135171332+LuizPretue@users.noreply.github.com>
2023-06-28 21:21:00 +0000

    calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -7,14 +6,44 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule],
 })
 export class HomePage {
+  resultado: number = 0;
+  value0: number = 0;
+  value1: number = 1;
+  value2: number = 2;
+  value3: number = 3;
+  value4: number = 4;
+  value5: number = 5;
+  value6: number = 6;
+  value7: number = 7;
+  value8: number = 8;
+  value9: number = 9;
 
-  selected: string = 'ads';
+  soma() {
+    this.resultado = Number(this.value1) + Number(this.value2);
+  }
 
+  subtrair() {
+    this.resultado = Number(this.value1) - Number(this.value2);
+  }
 
-  trocarCurso(event: any){
-    this.selected = event.detail.value;
+  mult() {
+    this.resultado = Number(this.value1) * Number(this.value2);
+  }
+
+  dividir() {
+    if (Number(this.value2) != 0) {
+      this.resultado = Number(this.value1) / Number(this.value2);
+    }
+    else{
+      alert('Não é possivel efetuar divisão por 0');
+    }
+  }
+
+  calcular() {
+    this.resultado;
   }
 }
+
