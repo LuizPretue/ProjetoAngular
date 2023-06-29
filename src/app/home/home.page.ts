5557fff57c97c041ea62cf94960a115fcdc54526
LuizFCSantos <135171332+LuizPretue@users.noreply.github.com>
2023-06-29 02:27:27 +0000

    funcao click

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NumericValueAccessor } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,41 +9,8 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
-  resultado: number = 0;
-  value0: number = 0;
-  value1: number = 1;
-  value2: number = 2;
-  value3: number = 3;
-  value4: number = 4;
-  value5: number = 5;
-  value6: number = 6;
-  value7: number = 7;
-  value8: number = 8;
-  value9: number = 9;
+  
+  
 
-  soma() {
-    this.resultado = Number(this.value1) + Number(this.value2);
-  }
-
-  subtrair() {
-    this.resultado = Number(this.value1) - Number(this.value2);
-  }
-
-  mult() {
-    this.resultado = Number(this.value1) * Number(this.value2);
-  }
-
-  dividir() {
-    if (Number(this.value2) != 0) {
-      this.resultado = Number(this.value1) / Number(this.value2);
-    }
-    else{
-      alert('Não é possivel efetuar divisão por 0');
-    }
-  }
-
-  calcular() {
-    this.resultado;
-  }
 }
 
