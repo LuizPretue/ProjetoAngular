b0a4a25d735791232e96abe0357f777b9e5b1074
LuizFCSantos <135171332+LuizPretue@users.noreply.github.com>
2023-06-30 00:51:34 +0000

    calculadora pronta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule, NumericValueAccessor } from '@ionic/angular';
+import { clear } from 'console';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,44 @@ import { IonicModule, NumericValueAccessor } from '@ionic/angular';
 })
 export class HomePage {
   
-  
+  resultado = '0';
+  valoratual = '';
+  conta: number = 0;
+  resultados: number = 0;
 
+  num(value: string){  
+    this.valoratual = this.valoratual + value    
+    this.resultado = this.valoratual
+  }
+  calcular(){
+    this.resultado = eval(this.valoratual)
+    this.valoratual = this.resultado
+  }
+  raiz(){
+    const raiz = Math.sqrt(this.conta);
+    this.resultados = this.conta;
+  }
+  deletar() {
+    this.valoratual = '';
+    this.resultado = '0';
+  }
+  backspace(){
+    // Slice retorna uma copia da metade de um array a partir de um subarray criado entre as posiçoes de um array original 
+    this.valoratual = this.valoratual.slice(0, -1)
+    this.resultado = this.valoratual
+   if(this.resultado==''){this.resultado='0'}
+  }
+  //  calcvalue(solve: any){
+  //   // charAt retorna um caracter de uma localização espesifica de uma string
+  //   if(solve.charAt(0) == '0'){
+  //     solve = solve.slice(1,)
+  //   }
+  //   this.resultado = eval(solve)
+  // }
+  pi(){
+
+  }
+  
+  
 }
 
